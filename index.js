52e0c169266bcff66ac74457d76ea5f78bbdc398
Drew <dch528@gmail.com>
2024-01-30 12:32:27 -0500

    Drew Hairston Arrow Functions Lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (nums1, nums2) => {
+console.log(`Dividing ${nums1}`);
+console.log(`Dividing ${nums2}`);
+    return 2000 / 100
+}
+
+
+const square = (x) => {
+return x * x
+}
+let x = 2
+
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
